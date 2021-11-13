17638f6dd619410fe3bdc866f617cecb4e296289
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-11-13 12:10:22 -0500

    Updated useCollection param type to correct type

--- packages/participant/src/hooks/useCollection.ts
+++ packages/participant/src/hooks/useCollection.ts
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
-function useCollection<T>(query: firebase.firestore.CollectionReference) {
+function useCollection<T>(query: firebase.firestore.Query<firebase.firestore.DocumentData>) {
   return useCollectionData<T, "id", "ref">(query, { idField: "id", refField: "ref" });
 }
 
