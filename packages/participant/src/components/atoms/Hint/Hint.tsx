5544995509d4ead1a750f83cd315e28bc3832634
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-11-15 16:15:47 -0500

    Allow passing of additional props to Hint

--- packages/participant/src/components/atoms/Hint/Hint.tsx
+++ packages/participant/src/components/atoms/Hint/Hint.tsx
@@ -6,19 +6,20 @@ import { IconType } from "react-icons/lib";
 import { ColorScheme } from "types/global";
 
 interface Props {
-  label: string;
+  label: string | React.ReactElement;
   icon?: IconType;
   colorScheme?: ColorScheme;
+  [key: string]: any;
 }
 
 export const Hint = React.memo(
-  ({ label, icon = FaExclamationCircle, colorScheme = "gray" }: Props) => {
+  ({ label, icon = FaExclamationCircle, colorScheme = "gray", ...rest }: Props) => {
     const tooltipBackground = useColor("gray.700", `${colorScheme}.400`);
 
     return (
       <Tooltip label={label} background={tooltipBackground}>
         <Flex align="center">
-          <Icon color={`${colorScheme}.400`} as={icon} />
+          <Icon color={`${colorScheme}.400`} as={icon} {...rest} />
         </Flex>
       </Tooltip>
     );
