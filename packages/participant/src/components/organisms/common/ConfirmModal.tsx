68cd729199966c6e33e2577e02ad07b267c7adaa
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-11-16 18:48:11 -0500

    Added ConfirmModal component

---
+++ packages/participant/src/components/organisms/common/ConfirmModal.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { useColor } from "hooks";
+
+import {
+  Box,
+  Flex,
+  Text,
+  Heading,
+  Button,
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from "@chakra-ui/react";
+import { SecondaryButton } from "components/atoms";
+import { Confirm } from "context/ConfirmContext";
+
+interface Props extends Confirm {
+  handleClose: () => void;
+}
+
+function ConfirmModal({
+  title,
+  description,
+  colorScheme,
+  buttonText,
+  handleClose,
+  handleConfirm,
+}: Props) {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    return handleConfirm().finally(() => {
+      handleClose();
+      setLoading(false);
+    });
+  };
+
+  const textColor = useColor("gray.500", "gray.400");
+  const backgroundColor = useColor("white", "gray.800");
+
+  return (
+    <AlertDialog
+      size="lg"
+      motionPreset="scale"
+      isCentered
+      isOpen={true}
+      onClose={loading ? () => {} : handleClose}
+      leastDestructiveRef={undefined}
+    >
+      <AlertDialogOverlay background="rgb(0, 0, 0, 0.75)">
+        <AlertDialogContent background={backgroundColor}>
+          <Box padding="20px" width="100%">
+            <Heading fontSize="lg" marginBottom="8px">
+              {title}
+            </Heading>
+            <Text color={textColor}>{description}</Text>
+            <Flex marginTop="24px" gridGap="10px" justify="flex-end">
+              <SecondaryButton isDisabled={loading} onClick={handleClose}>
+                Cancel
+              </SecondaryButton>
+              <Button
+                isLoading={loading}
+                colorScheme={colorScheme}
+                loadingText={buttonText}
+                onClick={handleSubmit}
+              >
+                {buttonText}
+              </Button>
+            </Flex>
+          </Box>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  );
+}
+
+export default ConfirmModal;
