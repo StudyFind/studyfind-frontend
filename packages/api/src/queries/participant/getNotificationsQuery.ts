95e4666074772b25b019d972578cfa628b6d7c74
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-11-18 20:29:00 -0500

    Cleaned up

--- packages/api/src/queries/participant/getNotificationsQuery.ts
+++ packages/api/src/queries/participant/getNotificationsQuery.ts
@@ -2,6 +2,9 @@ import { auth, firestore } from "@studyfind/firebase";
 
 export const getNotificationsQuery = () => {
   const participantID = auth.getUser().uid;
+
+  console.log(participantID);
+
   return firestore.references
     .getParticipantNotificationsReference(participantID)
     .orderBy("createdAt", "desc");
