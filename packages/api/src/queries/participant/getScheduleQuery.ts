0cbe958a852d177e1d7f9dc40947391d4e578bae
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-11-21 15:11:23 -0500

    Fixed scheduleQuery

--- packages/api/src/queries/participant/getScheduleQuery.ts
+++ packages/api/src/queries/participant/getScheduleQuery.ts
@@ -2,10 +2,13 @@ import firebase from "firebase";
 import { auth, services } from "@studyfind/firebase";
 import { MeetingDocument } from "@studyfind/types";
 
-export const getScheduleQuery = () => {
+export const getScheduleQuery = (start: number, end: number) => {
   const participantID = auth.getUser().uid;
 
   return services.firestore
     .collectionGroup("meetings")
-    .where("participantID", "==", participantID) as firebase.firestore.Query<MeetingDocument>;
+    .where("participantID", "==", participantID)
+    .where("time", ">=", start)
+    .where("time", "<=", end)
+    .orderBy("time", "asc") as firebase.firestore.Query<MeetingDocument>;
 };
--- packages/api/src/queries/researcher/getScheduleQuery.ts
+++ packages/api/src/queries/researcher/getScheduleQuery.ts
@@ -2,10 +2,13 @@ import firebase from "firebase";
 import { auth, services } from "@studyfind/firebase";
 import { MeetingDocument } from "@studyfind/types";
 
-export const getScheduleQuery = () => {
+export const getScheduleQuery = (start: number, end: number) => {
   const researcherID = auth.getUser().uid;
 
   return services.firestore
     .collectionGroup("meetings")
-    .where("researcherID", "==", researcherID) as firebase.firestore.Query<MeetingDocument>;
+    .where("researcherID", "==", researcherID)
+    .where("time", ">=", start)
+    .where("time", "<=", end)
+    .orderBy("time", "asc") as firebase.firestore.Query<MeetingDocument>;
 };
