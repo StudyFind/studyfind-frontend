a832776e284e2933f6e7804f47eaa680e9e3aa2f
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-11-22 22:11:04 -0500

    Removed redundant file

--- packages/participant/src/components/templates/Internal/Schedule/types.ts
+++
@@ -1,4 +0,0 @@
-import { Data } from "react-firebase-hooks/firestore/dist/firestore/types";
-import { MeetingDocument } from "@studyfind/types";
-
-export type MeetingDocumentExtended = Data<MeetingDocument, "id", "ref">;
