7c198b324aec90955c555e9df43784773adede80
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-11-23 18:58:39 -0500

    Removed redundant ternary

--- packages/participant/src/pages/Internal/FindStudies/AutoScroll.tsx
+++ packages/participant/src/pages/Internal/FindStudies/AutoScroll.tsx
@@ -1,18 +1,14 @@
 import { FaArrowUp } from "react-icons/fa";
 import { IconButton } from "@chakra-ui/react";
-import { useWindowSize } from "react-use";
 
 interface Props {
   onClick: () => void;
 }
 
 function AutoScroll({ onClick }: Props) {
-  const { height } = useWindowSize();
-
   return (
     <IconButton
       onClick={onClick}
-      display={height > 100 ? "flex" : "none"}
       position="fixed"
       right="16px"
       bottom="16px"
