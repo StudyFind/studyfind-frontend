bc11f035f5cdaf99dcf3dc05b21baad8739b0304
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-11-22 22:05:12 -0500

    Added export for organisms

---
+++ packages/participant/src/components/organisms/index.ts
@@ -0,0 +1,6 @@
+export { default as StudyCardLarge } from "./common/StudyCard/StudyCardLarge";
+export { default as ConfirmModal } from "./common/ConfirmModal";
+export { default as VerificationBanner } from "./common/VerificationBanner/VerificationBanner";
+export { default as Toolbar } from "./common/Toolbar/Toolbar";
+export { default as Sidebar } from "./common/Sidebar/Sidebar";
+export { default as Router } from "./common/Router";
