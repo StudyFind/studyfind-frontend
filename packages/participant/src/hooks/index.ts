91a39b29bf0c906c7a8ed2815e38687fc79ece0f
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-11-26 14:20:16 -0500

    Added useDebounce hook

--- packages/participant/src/hooks/index.ts
+++ packages/participant/src/hooks/index.ts
@@ -2,6 +2,7 @@ export { useColorModeValue as useColor, useClipboard } from "@chakra-ui/react";
 export { default as usePagination } from "./usePagination";
 export { default as useCollection } from "./useCollection";
 export { default as useDocument } from "./useDocument";
+export { default as useDebounce } from "./useDebounce";
 export { default as useAuthForm } from "./useAuthForm";
 export { default as useOverlay } from "./useOverlay";
 export { default as useDevice } from "./useDevice";
